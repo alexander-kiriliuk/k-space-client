33db512b69fed0bbc3bf1695e3f03b8c3d7eb32a
Alexander <0074.sanu@gmail.com>
2024-09-17 00:27:55 +0300

    Added basic file system reader for android with special permissions

--- android/app/src/main/java/com/kspace/client/MainActivity.java
+++ android/app/src/main/java/com/kspace/client/MainActivity.java
@@ -1,5 +1,38 @@
 package com.kspace.client;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    requestForStoragePermissions();
+  }
+
+  private void requestForStoragePermissions() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+      if (Environment.isExternalStorageManager()) {
+        return;
+      }
+      try {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+        Uri uri = Uri.fromParts("package", this.getPackageName(), null);
+        intent.setData(uri);
+        startActivity(intent);
+      } catch (Exception e) {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+        startActivity(intent);
+      }
+    }
+  }
+
+}
--- src/app/pages/home/home-page.component.ts
+++ src/app/pages/home/home-page.component.ts
@@ -1,5 +1,6 @@
-import {Component} from "@angular/core";
+import {Component, OnInit, signal} from "@angular/core";
 import {IonContent} from "@ionic/angular/standalone";
+import {Directory, FileInfo, Filesystem} from "@capacitor/filesystem";
 
 @Component({
   selector: "home-page",
@@ -10,6 +11,40 @@ import {IonContent} from "@ionic/angular/standalone";
     IonContent
   ]
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+
+  dirs = signal<FileInfo[]>(undefined);
+  currentDir = signal("");
+
+  ngOnInit() {
+    this.readDir();
+  }
+
+  readDir(dirName = "") {
+    Filesystem.requestPermissions().then(result => {
+      if (result.publicStorage !== "granted") {
+        // todo alert
+        return;
+      }
+      const requestedDir = this.currentDir() + "/" + dirName;
+      console.debug(`Attempting to read directory: ${requestedDir}`);
+      Filesystem.readdir({
+        path: requestedDir,
+        directory: Directory.ExternalStorage
+      }).then(res => {
+        if (res.files.length === 0) {
+          console.debug("Directory is empty or cannot be read.");
+        } else {
+          this.currentDir.set(requestedDir.replace(/^\/+|\/+$/g, ""));
+          console.debug("Files found:", res.files);
+        }
+        this.dirs.set(res.files);
+      }).catch(err => {
+        console.error("Error while reading directory:", err);
+      });
+    }).catch(err => {
+      console.error("Permissions err:", err);
+    });
+  }
 
 }
